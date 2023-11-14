031e4f4ab487232af825dbf69e1c11158b2c410e
Kent 'picat' Gruber <kent@hashicorp.com>
2023-11-14 14:17:56 -0500

    Update metadata-startup-script.sh

--- metadata-startup-script.sh
+++ metadata-startup-script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Step 1: Get Public IP
-PUBLIC_UP=`curl --silent http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip -H 'Metadata-Flavor: Google'`
+# Install ops agent
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
-# Step 2: Install Docker Daemon
+# Install Docker Daemon
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
@@ -11,8 +12,10 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-ce
 sudo systemctl enable docker
 sudo systemctl start docker
 
-# Step 3: Install QuakJS Docker Container
+# Install QuakJS Docker Container
 sudo docker pull treyyoder/quakejs:latest
 
-# Step 4: Run QuakeJS Docker Container
-sudo docker run -d --name quakejs -e SERVER=$PUBLIC_UP -e HTTP_PORT=80 -p 80:80 -p 27960:27960 treyyoder/quakejs:latest
+PUBLIC_UP=`curl --silent http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip -H 'Metadata-Flavor: Google'`
+
+# Run QuakeJS Docker Container
+sudo docker run -d --name quakejs -e SERVER=$PUBLIC_IP -e HTTP_PORT=80 -p 80:80 -p 27960:27960 treyyoder/quakejs:latest
